c5b4bcd0a1e7a237cafce5d550a8bb1bfa49eaec
LyHorng <e20220188@dtc1.itc.edu.kh>
2026-03-06 00:24:15 +0700

    Modify controller and service of task with main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,22 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS for Vue frontend
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,POST,PATCH,DELETE',
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       transform: true,
     }),
   );
+
   await app.listen(process.env.PORT ?? 3100);
 }
+
 bootstrap();
--- src/modules/task/task.service.ts
+++ src/modules/task/task.service.ts
@@ -2,6 +2,31 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class TaskService {
+  getAllTasks() {
+    return [
+      {
+        name: 'Task 1',
+        description: 'Description of Task 1',
+        createdAt: new Date().toISOString(),
+        completedAt: null,
+        userId: 1,
+      },
+      {
+        name: 'Task 2',
+        description: 'Description of Task 2',
+        createdAt: new Date().toISOString(),
+        completedAt: new Date().toISOString(),
+        userId: 1,
+      },
+      {
+        name: 'Task 3',
+        description: 'Description of Task 3',
+        createdAt: new Date().toISOString(),
+        completedAt: null,
+        userId: 1,
+      }
+    ];
+  }
   getTask(id: string) {
     console.log(id);
     return {
