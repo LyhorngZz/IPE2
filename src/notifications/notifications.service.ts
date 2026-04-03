3e8e30154ec0a988aea993c122fe12d1957cf7ce
LyHorng <e20220188@dtc1.itc.edu.kh>
2026-04-03 08:13:49 +0700

    Clean code

--- src/notifications/notifications.service.ts
+++ src/notifications/notifications.service.ts
@@ -13,15 +13,14 @@ export class NotificationsService {
     //     private readonly ordersService: OrdersService,
     // ) { }
 
-    constructor(
-        @Inject(EVENT_PUBLISHER)
-        private readonly publisher: EventPublisher,
-    ) { }
-
     // notify(event: string, payload: any) {
     //     console.log(`[NOTIFY] ${event}`, payload);
     //     return { ok: true };
     // }
+    constructor(
+        @Inject(EVENT_PUBLISHER)
+        private readonly publisher: EventPublisher,
+    ) { }
 
     notify(event: string, payload: any) {
         this.publisher.publish(event, payload);
